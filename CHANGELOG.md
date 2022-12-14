1164621997a549f26a6cd6eabf13cc9fbdaee7ed
reka <382113+reka@users.noreply.github.com>
2022-12-14 19:54:55 +0100

    fix: Support package names incl. dot fixes #7 (#8)
    Test cases:
    
    * `app.api`: No importers
    * `app.core`: only `app.api` can import

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.2.1] - 2022-12-14
+
+### Fixed
+
+* Support fully qualified package names incl. dot [GH-7](https://github.com/sourcery-ai/sourcery-rules-generator/issues/7) 
+
 ## [0.2.0] - 2022-12-13
 
 ### Added
--- sourcery_rules_generator/dependencies.py
+++ sourcery_rules_generator/dependencies.py
@@ -13,7 +13,13 @@ def create_yaml_rules(package: str, allowed_importer: Optional[str]) -> str:
 
 
 def create_sourcery_custom_rules(package: str, allowed_importer: Optional[str]) -> str:
-    exclude_paths = [f"{package}/", "tests/"]
+    # Dots aren't allowed in the rule ID.
+    package_slug = package.replace(".", "-")
+
+    package_in_regex = package.replace(".", "\.")
+    package_path = package.replace(".", "/")
+
+    exclude_paths = [f"{package_path}/", "tests/"]
     description = f"Do not import `{package}` in other packages"
     if allowed_importer:
         description = f"Only `{allowed_importer}` should import `{package}`"
@@ -21,20 +27,20 @@ def create_sourcery_custom_rules(package: str, allowed_importer: Optional[str]) 
         exclude_paths.append(f"{allowed_importer_path}/")
 
     import_rule = SourceryCustomRule(
-        id=f"dependency-rules-{package}-import",
+        id=f"dependency-rules-{package_slug}-import",
         description=description,
         tags=["architecture", "dependencies"],
         pattern="import ..., ${module}, ...",
-        condition=f'module.matches_regex(r"^{package}")',
+        condition=f'module.matches_regex(r"^{package_in_regex}")',
         paths=PathsConfig(exclude=exclude_paths),
     )
 
     from_rule = SourceryCustomRule(
-        id=f"dependency-rules-{package}-from",
+        id=f"dependency-rules-{package_slug}-from",
         description=description,
         tags=["architecture", "dependencies"],
         pattern="from ${module} import ...",
-        condition=f'module.matches_regex(r"^{package}")',
+        condition=f'module.matches_regex(r"^{package_in_regex}")',
         paths=PathsConfig(exclude=exclude_paths),
     )
 
