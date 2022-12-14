3ee18f3d0c86254a4727401449c61d392cc316e5
reka <reka@hey.com>
2022-12-14 17:38:41 +0100

    fix: Support package names incl. dot fixes #7
    Test cases:
    
    * `app.api`: No importers
    * `app.core`: only `app.api` can import

--- tests/unit/test_dependencies.py
+++ tests/unit/test_dependencies.py
@@ -50,3 +50,53 @@ def test_0_allowed_importer():
     )
 
     assert expected == result
+
+
+def test_1_allowed_importer_package_name_incl_dot():
+    result = dependencies.create_sourcery_custom_rules("app.core", "app.api")
+
+    expected = (
+        SourceryCustomRule(
+            id="dependency-rules-app-core-import",
+            description="Only `app.api` should import `app.core`",
+            tags=["architecture", "dependencies"],
+            pattern="import ..., ${module}, ...",
+            condition='module.matches_regex(r"^app\.core")',
+            paths=PathsConfig(exclude=["app/core/", "tests/", "app/api/"]),
+        ),
+        SourceryCustomRule(
+            id="dependency-rules-app-core-from",
+            description="Only `app.api` should import `app.core`",
+            tags=["architecture", "dependencies"],
+            pattern="from ${module} import ...",
+            condition='module.matches_regex(r"^app\.core")',
+            paths=PathsConfig(exclude=["app/core/", "tests/", "app/api/"]),
+        ),
+    )
+
+    assert expected == result
+
+
+def test_0_allowed_importer_package_name_with_dot():
+    result = dependencies.create_sourcery_custom_rules("app.api", "")
+
+    expected = (
+        SourceryCustomRule(
+            id="dependency-rules-app-api-import",
+            description="Do not import `app.api` in other packages",
+            tags=["architecture", "dependencies"],
+            pattern="import ..., ${module}, ...",
+            condition='module.matches_regex(r"^app\.api")',
+            paths=PathsConfig(exclude=["app/api/", "tests/"]),
+        ),
+        SourceryCustomRule(
+            id="dependency-rules-app-api-from",
+            description="Do not import `app.api` in other packages",
+            tags=["architecture", "dependencies"],
+            pattern="from ${module} import ...",
+            condition='module.matches_regex(r"^app\.api")',
+            paths=PathsConfig(exclude=["app/api/", "tests/"]),
+        ),
+    )
+
+    assert expected == result
